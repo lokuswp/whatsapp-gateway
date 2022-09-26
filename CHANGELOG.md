5613a25cab5a3ce6fb1faeae564aab5b9e4bb861
lasida <lasidaziz@gmail.com>
2022-09-26 12:03:28 +0700

    RC-0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -28,3 +28,10 @@
 
 **Newest :**
 - Product Archive
+
+# [v0.2.0]() (2022-09-26)
+**Fixing :**
+- Pickup Notification
+
+**Newest :**
+- Plugin Updater
--- whatsapp-gateway.php
+++ whatsapp-gateway.php
@@ -4,10 +4,10 @@
  * @wordpress-plugin
  * @lokuswp-integration
  *
- * Plugin Name:       🪝️ LokusWP - Whatsapp Gateway
+ * Plugin Name:       ⚏ LokusWP - Whatsapp Gateway
  * Plugin URI:        lokuswp.id/plugins/lokuswp/whatsapp-gateway
- * Description:       Kirim Notifikasi via Whatsapp dengan Gateway yang kamu suka
- * Version:           0.1.2
+ * Description:       [Addon] Integrasi Notifikasi Whatsapp
+ * Version:           0.2.0
  * Author:            LokusWP
  * Author URI:        https://lokuswp.id/
  * License:           GPL-3.0+
@@ -26,69 +26,55 @@ if ( ! defined( 'WPTEST' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  * Define Constant
  */
-defined( 'LOKUSWP_WA_GATEWAY_VERSION' ) or define( 'LOKUSWP_WA_GATEWAY_VERSION', '0.1.2' );
+defined( 'LOKUSWP_WA_GATEWAY_VERSION' ) or define( 'LOKUSWP_WA_GATEWAY_VERSION', '0.2.0' );
+defined( 'LOKUSWP_WA_GATEWAY_REQUIRED_VERSION' ) or define( 'LOKUSWP_WA_GATEWAY_REQUIRED_VERSION', '0.1.7' );
 defined( 'LOKUSWP_WA_GATEWAY_BASE' ) or define( 'LOKUSWP_WA_GATEWAY_BASE', plugin_basename( __FILE__ ) );
 defined( 'LOKUSWP_WA_GATEWAY_PATH' ) or define( 'LOKUSWP_WA_GATEWAY_PATH', plugin_dir_path( __FILE__ ) );
 defined( 'LOKUSWP_WA_GATEWAY_URI' ) or define( 'LOKUSWP_WA_GATEWAY_URI', plugin_dir_url( __FILE__ ) );
 
 /**
- * Dependency Backbone Checking
+ *-----------------------*
+ * Minimum Requirement System
+ * PHP : 7.4
+ * WordPress : 5.9
+ * LokusWP : 0.1.7
  *
- * @return void
- */
-function lwp_wa_gateway_dependency() {
-	$lwp_active  = true;
-	$lwp_version = true;
+ * @since 0.1.0
+ *-----------------------*
+ **/
+$is_backbone_exist = file_exists( WP_PLUGIN_DIR . '/lokuswp/lokuswp.php' );
+$backbone_version  = $is_backbone_exist ? get_file_data( WP_PLUGIN_DIR . '/lokuswp/lokuswp.php', array( 'Version' ), false )[0] : false;
 
-	// is LokusWP Active ??
-	if ( is_admin() && current_user_can( 'activate_plugins' ) && ! is_plugin_active( 'lokuswp/lokuswp.php' ) ) {
-
+if ( $is_backbone_exist && version_compare( $backbone_version, LOKUSWP_WA_GATEWAY_REQUIRED_VERSION, '<' ) ) {
+	add_action( 'admin_notices', 'lwp_wa_gateway_fail_lokuswp_version' );
+} else {
+	// Come On, Let's Goo !!! 🦾
+	if ( ! file_exists( LOKUSWP_WA_GATEWAY_PATH . '/src/autoload.php' ) ) {
 		add_action( 'admin_notices', function () {
-			echo '<div class="error"><p>' . __( 'LokusWP required. please activate plugin first.', 'lokuswp-wa_gateway' ) . '</p></div>';
-		} );
-
-		$lwp_active = false;
-	}
-
-	// Checking LokusWP Version to Run this Plugin
-	$lwp_version = get_plugin_data( dirname( dirname( __FILE__ ) ) . '/lokuswp/lokuswp.php' );
-	$lwp_version = $lwp_version['Version'] ?? false;
-	if ( ! version_compare( LOKUSWP_WA_GATEWAY_VERSION, $lwp_version, '<' ) ) {
-
-		add_action( 'admin_notices', function () use ( $lwp_version ) {
-			$message      = sprintf( esc_html__( 'LokusWP WA_Gateway anda tidak kompatibel dengan versi LokusWP Backbone saat ini, silahkan gunakan LokusWP WA_Gateway v%s atau dibawahnya', 'lokuswp-wa_gateway' ),
-				$lwp_version );
+			$message      = sprintf( esc_html__( 'LokusWP WA_Gateway :: Nama folder slug anda tidak sesuai dengan nama slug plugin, pastikan nama folder plugin anda %s', 'lokuswp-wa_gateway' ), 'whatsapp-gateway/whatsapp-gateway.php' );
 			$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
 			echo wp_kses_post( $html_message );
 		} );
+	} else {
 
-		$lwp_version = false;
-	}
-
-	// When not Right -> Deactivate Extension
-	if ( ! $lwp_version || ! $lwp_active ) {
-		deactivate_plugins( plugin_basename( __FILE__ ) );
-
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
+		// Run LokusWP WA_Gateway
+		if ( in_array( 'lokuswp/lokuswp.php', (array) apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+			require_once LOKUSWP_WA_GATEWAY_PATH . '/src/autoload.php';
 		}
 	}
 }
 
-add_action( 'admin_init', 'lwp_wa_gateway_dependency' );
-
-// Checking : Slug Folder match with Plugin SLug
-// Comment this if you want to test directly on your localhost, Uncomment for Production Ready
-if ( ! file_exists( LOKUSWP_WA_GATEWAY_PATH . '/src/autoload.php' ) ) {
-	add_action( 'admin_notices', function () {
-		$message      = sprintf( esc_html__( 'LokusWP WA_Gateway :: Nama folder slug anda tidak sesuai dengan nama slug plugin, harap ganti nama folder plugin anda dengan %s', 'lokuswp-wa_gateway' ), 'lokuswp-wa_gateway' );
-		$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
-		echo wp_kses_post( $html_message );
-	} );
-} else {
-
-	// Run LokusWP WA_Gateway
-	if ( in_array( 'lokuswp/lokuswp.php', (array) apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-		require_once LOKUSWP_WA_GATEWAY_PATH . '/src/autoload.php';
-	}
+/**
+ * Admin notice for minimum LokusWP version.
+ * Warning when the site doesn't have the minimum required LokusWP version.
+ *
+ * @return void
+ * @since 0.1.0
+ */
+function lwp_wa_gateway_fail_lokuswp_version() {
+	/* translators: %s: WordPress version */
+	$message      = sprintf( esc_html__( '[Addon] Whatsapp Gateway active but not working. required LokusWP %s+ to be active', 'whatsapp-gateway' ), LOKUSWP_WA_GATEWAY_REQUIRED_VERSION );
+	$html_message = sprintf( '<div class="error">%s</div>', wpautop( $message ) );
+	deactivate_plugins( WP_PLUGIN_DIR . '/lokuswp/lokuswp.php' );
+	echo wp_kses_post( $html_message );
 }
